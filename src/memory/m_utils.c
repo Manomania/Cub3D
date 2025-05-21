dc86d6492fb358c86317082c9e13346f40912a10
maximart <maxime.martinez96@hotmail.fr>
2025-05-21 15:46:41 +0200

    refactor(free): memory management and resource cleanup.

--- src/memory/m_utils.c
+++ src/memory/m_utils.c
@@ -33,58 +33,12 @@ bool	check_args(int argc, char **argv)
 	return (false);
 }
 
-t_data	*init_data(void)
-{
-	t_data	*data;
-
-	data = ft_calloc(1, sizeof(t_data));
-	data->floor_color[0] = -1;
-	data->floor_color[1] = -1;
-	data->floor_color[2] = -1;
-	data->ceil_color[0] = -1;
-	data->ceil_color[1] = -1;
-	data->ceil_color[2] = -1;
-	return (data);
-}
-
 bool	init_map_array(t_data *data)
 {
-	data->map = ft_calloc(data->map_height + 1,
-			sizeof(char *) * data->map_height + 1);
+	if (data->map_height < 1)
+		return (true);
+	data->map = ft_calloc(data->map_height + 1, sizeof(char *));
 	if (!data->map)
 		return (true);
 	return (false);
-}
-
-void	free_ressources(t_data *data)
-{
-	if (!data)
-		return ;
-	free_map(data);
-	if (data->texture_n)
-		free(data->texture_n);
-	if (data->texture_s)
-		free(data->texture_s);
-	if (data->texture_w)
-		free(data->texture_w);
-	if (data->texture_e)
-		free(data->texture_e);
-	free(data);
-	exit(1);
-}
-
-void	free_map(t_data *data)
-{
-	int	i;
-
-	if (!data->map)
-		return ;
-	i = 0;
-	while (data->map[i])
-	{
-		free(data->map[i]);
-		i++;
-	}
-	free(data->map);
-	data->map = NULL;
 }
