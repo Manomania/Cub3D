3c8430fd66cefc5cb5ae405388ad0a3b8651e6c3
maximart <maxime.martinez96@hotmail.fr>
2025-06-02 17:53:39 +0200

    feat(mouse): add mouse config struct and constants

--- include/mouse.h
+++ include/mouse.h
@@ -19,12 +19,35 @@
  *                                    Macros                                   *
  ******************************************************************************/
 
+# define MOUSE_SENSITIVITY 0.002
+# define MOUSE_MIN 0.0005
+# define MOUSE_MAX 0.01
+# define MOUSE_LEFT_CLICK 1
+# define MOUSE_RIGHT_CLICK 3
+# define MOUSE_SCROLL_UP 4
+# define MOUSE_SCROLL_DOWN 5
+
 /*******************************************************************************
  *                                  Structures                                 *
  ******************************************************************************/
 
+typedef struct s_mouse
+{
+	int		x;
+	int		y;
+	int		last_x;
+	int		last_y;
+	bool	is_active;
+	bool	first_move;
+	double	sensitivity;
+}	t_mouse;
+
 /*******************************************************************************
  *                             Function Prototypes                             *
  ******************************************************************************/
+
+void	mouse_toggle(void);
+void	mouse_init(int win_width, int win_height);
+void	mouse_setup_hooks(t_data *data);
 
 #endif
