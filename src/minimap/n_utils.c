31c69d2c437705718196d73ab4647ccdf9b102e3
maximart <maxime.martinez96@hotmail.fr>
2025-06-02 12:55:35 +0200

    refactor(minimap): remove unused functions and simplify logic

--- src/minimap/n_utils.c
+++ src/minimap/n_utils.c
@@ -13,25 +13,6 @@
 #include "minimap.h"
 #include "draw.h"
 
-void	draw_border_lines(t_data *data, t_draw_params *params)
-{
-	params->x = params->minimap_x;
-	params->y = params->minimap_y;
-	params->width = params->minimap_x + MINIMAP_SIZE;
-	params->height = params->minimap_y;
-	draw_line(data, params);
-	params->y = params->minimap_y + MINIMAP_SIZE;
-	params->height = params->minimap_y + MINIMAP_SIZE;
-	draw_line(data, params);
-	params->width = params->minimap_x;
-	params->height = params->minimap_y + MINIMAP_SIZE;
-	draw_line(data, params);
-	params->x = params->minimap_x + MINIMAP_SIZE;
-	params->width = params->minimap_x + MINIMAP_SIZE;
-	params->height = params->minimap_y + MINIMAP_SIZE;
-	draw_line(data, params);
-}
-
 bool	check_fov_bounds(t_draw_params *params, int screen_x, int screen_y)
 {
 	if (screen_x < params->minimap_x
@@ -51,32 +32,30 @@ bool	is_player_in_bounds(t_draw_params *params)
 	return (false);
 }
 
-static void	draw_single_fov_line(t_data *data, t_draw_params *params,
-		int *player_screen, int end_x, int end_y)
+int	is_pixel_valid(t_data *data, int x, int y)
 {
-	params->x = player_screen[0];
-	params->y = player_screen[1];
-	params->width = end_x;
-	params->height = end_y;
-	draw_line(data, params);
+	if (x >= 0 && y >= 0 && x < data->win_width && y < data->win_height)
+		return (1);
+	return (0);
 }
 
-void	draw_player_fov(t_data *data, t_draw_params *params, int *player_screen)
+void	init_steps(t_draw_params *params, int *step_x, int *step_y)
 {
-	double	player_angle;
-	int		fov_ends[4];
+	if (params->width > params->x)
+		*step_x = 1;
+	else
+		*step_x = -1;
+	if (params->height > params->y)
+		*step_y = 1;
+	else
+		*step_y = -1;
+}
 
-	params->color.val = MINIMAP_FOV_COLOR;
-	player_angle = atan2(data->player.dir_y, data->player.dir_x);
-	cast_fov_ray(data, params, player_angle - 0.5);
-	fov_ends[0] = params->end_x;
-	fov_ends[1] = params->end_y;
-	cast_fov_ray(data, params, player_angle + 0.5);
-	fov_ends[2] = params->end_x;
-	fov_ends[3] = params->end_y;
-	cast_fov_ray(data, params, player_angle);
-	draw_single_fov_line(data, params, player_screen, fov_ends[0], fov_ends[1]);
-	draw_single_fov_line(data, params, player_screen, fov_ends[2], fov_ends[3]);
-	draw_single_fov_line(data, params, player_screen, params->end_x,
-		params->end_y);
+void	init_ray_pos(t_data *data, double angle, double *ray_pos,
+		double *ray_dir)
+{
+	ray_pos[0] = data->player.pos_x;
+	ray_pos[1] = data->player.pos_y;
+	ray_dir[0] = cos(angle) * 0.02;
+	ray_dir[1] = sin(angle) * 0.02;
 }
