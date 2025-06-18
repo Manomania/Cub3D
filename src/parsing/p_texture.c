734dfc60e06c3e03f602ff9a094a0e863634e83f
maximart <maxime.martinez96@hotmail.fr>
2025-06-18 10:38:52 +0200

    fix(crash): Enhanced the exit error of wrong/duplicate texture

--- src/parsing/p_texture.c
+++ src/parsing/p_texture.c
@@ -13,6 +13,18 @@
 #include "libft.h"
 #include "parsing.h"
 
+static bool	check_text_duplicate(t_data *data, const char *cardinal, char *exist)
+{
+	if (exist)
+	{
+		data->error_detected = true;
+		ft_printf(RED"Error\nDuplicate texture for %s\n"RESET, cardinal);
+		return (true);
+	}
+	return (false);
+}
+
+
 bool	parse_texture_path(t_data *data, char *line, const char *cardinal)
 {
 	char	*skip;
@@ -27,15 +39,35 @@ bool	parse_texture_path(t_data *data, char *line, const char *cardinal)
 		while (*path && (*path == ' ' || *path == '\t'))
 			path++;
 		if (ft_strcmp(cardinal, "NO ") == 0)
+		{
+			if (check_text_duplicate(data, cardinal, data->texture_n))
+				return (false);
 			data->texture_n = ft_strtrim(path, "\n");
+		}
 		else if (ft_strcmp(cardinal, "SO ") == 0)
+		{
+			if (check_text_duplicate(data, cardinal, data->texture_s))
+				return (false);
 			data->texture_s = ft_strtrim(path, "\n");
+		}
 		else if (ft_strcmp(cardinal, "WE ") == 0)
+		{
+			if (check_text_duplicate(data, cardinal, data->texture_w))
+				return (false);
 			data->texture_w = ft_strtrim(path, "\n");
+		}
 		else if (ft_strcmp(cardinal, "EA ") == 0)
+		{
+			if (check_text_duplicate(data, cardinal, data->texture_e))
+				return (false);
 			data->texture_e = ft_strtrim(path, "\n");
+		}
 		else if (ft_strcmp(cardinal, "D ") == 0)
+		{
+			if (check_text_duplicate(data, cardinal, data->texture_door))
+				return (false);
 			data->texture_door = ft_strtrim(path, "\n");
+		}
 		return (false);
 	}
 	return (true);
